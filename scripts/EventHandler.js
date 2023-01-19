bd525a7bc085cf0e1a273d3529200ce632622add
Raymond-Doyle <raymond.doyle1@dcmail.ca>
2023-01-19 15:46:52 -0500

    Added other files

---
+++ scripts/EventHandler.js
@@ -0,0 +1,44 @@
+var welcomeLabel = document.getElementById("welcome");
+var displayEvent = document.getElementById("displayEvent");
+
+var userDisplay = document.getElementById("username");
+var passDisplay = document.getElementById("password");
+
+//Click Event
+welcomeLabel.addEventListener("click", function (){
+    console.log("Event: click");
+    displayEvent.textContent = "Event: click";
+} );
+
+//Double Click Event
+welcomeLabel.addEventListener("dblclick", function (){
+    console.log("Event: dblclick");
+    displayEvent.textContent = "Event: dblclick";
+} );
+
+//Mouse Over Events
+welcomeLabel.addEventListener("mouseover", function (){
+    console.log("Event: mouseover");
+    displayEvent.textContent = "Event: mouseover";
+});
+
+//Mouse Out Events
+welcomeLabel.addEventListener("mouseout", function (){
+    console.log("Event: mouseout");
+    displayEvent.textContent = "Event: mouseout";
+});
+
+function validateForm(){
+    let x = document.forms["myForm"]["username"].value;
+    let y = document.forms["myForm"]["password"].value;
+
+    if (x == ""){
+        alert("Name must be filled out");
+        return false;
+    } else {
+        console.log('Username: ${ x } \n Password: ${ y }');
+        userDisplay.textContent = x;
+        passDisplay.textContent = y;
+        return false;
+    }
+}
