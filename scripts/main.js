cdd350a8d4dc174a63b9a6fe20d19ecee6c8afd5
Raymond-Doyle <raymond.doyle1@dcmail.ca>
2023-01-26 15:34:09 -0500

    Final ICE 3

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,28 @@
         })
         let mainContent  = document.getElementsByTagName("main")[0]
         //console.log(mainContent)
+        mainContent.setAttribute("class", "container")
+        documentBody = document.body
 
         let mainParagraph = document.createElement("p")
         mainParagraph.setAttribute("id", "MainParagraph")
-        mainParagraph.setAttribute("class", "mt-3")
+        mainParagraph.setAttribute("class", "mt-3 container")
 
-        let firstString = "This is a paragraph that we added through javascript"
-        mainParagraph.textContent = firstString
+        let firstString = "This is a "
+        let secondString = `${firstString} paragraph that we added through javascript`
+        mainParagraph.textContent = secondString
 
-        mainContent.appendChild(mainParagraph)
+        mainContent.before(mainParagraph)
+        //mainContent.appendChild(mainParagraph)
+        //document.getElementById("randomButton").remove()
+
+        /*documentBody.innerHTML = `
+        <div class="container">
+            <h1 class="display-1">Hello WEBD</h1>
+            <p>This is the test page</p>
+        </div>`*/
+        
+    
     }
 
 
