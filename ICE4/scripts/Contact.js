9b80ccb2f371724416b59972a8c2da922a758182
Raymond-Doyle <raymond.doyle1@dcmail.ca>
2023-02-05 13:42:14 -0500

    Lecture Part 2 Complete ICE

--- ICE4/scripts/Contact.js
+++ ICE4/scripts/Contact.js
@@ -29,6 +29,25 @@ class Contact {
         this.m_emailAddress = emailAddress
     }
 
+    //Public Utility Method
+    serialize(){
+        if (this.Name !== "" && this.ContactNumber !== "" && this.EmailAddress !== ""){
+            return `${ this.Name }, ${ this.ContactNumber }, ${ this.EmailAddress }`
+        }
+
+        console.error("One or more properties or fields of the Contact Object are missing or invalid!")
+        return null
+    }
+
+    deserialize(data){
+
+        let propertyArray = data.split(", ")
+        this.Name = propertyArray[0]
+        this.ContactNumber = propertyArray[1]
+        this.EmailAddress = propertyArray[2]
+
+    }
+
 
     //Public Override Method
     toString(){
--- ICE4/scripts/main.js
+++ ICE4/scripts/main.js
@@ -31,19 +31,59 @@
         let submitButton = document.getElementById("submitButton")
         let subscribeCheckbox = document.getElementById("subscribeCheckbox")
 
+        //Local Storage
+        /*localStorage.setItem("Random Variable", "Random variable for testing and demonstration")
+        console.log(localStorage.getItem("Random Variable"))
+        localStorage.removeItem("Random Variable")*/
+
         console.log("Contacts Page")
 
 
         submitButton.addEventListener("click", function(event){
-            event.preventDefault
+            //event.preventDefault
             if (subscribeCheckbox.checked){
-                console.log("The user had subscribed")
+                //if the user subscribes store the contact in local storage
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value)
+                if (contact.serialize()){
+                    let key = contact.Name.substring(0, 1) + Date.now()
+                    localStorage.setItem(key, contact.serialize())
+                }
             }
         })
     }
 
     function DisplayContactList(){
-        console.log("Contact List Page")
+
+        if (localStorage.length > 0){
+
+            let contactList = document.getElementById("contactList") // Our contact list in the table of the contact list page
+            let data = "" //Add data to this variable. Append deserialized data from LocalStorage to data
+            let keys = Object.keys(localStorage)//Return a String Array of keys
+            let index = 1 // Count the number of keys
+            
+            //For every key in the keys collection
+            for (const key of keys) {
+                let contactData = localStorage.getItem(key) //Get localStorage data value related to the key
+                let contact = new Contact()
+                contact.deserialize(contactData)
+
+                //Inject repeatable row into the contact list
+                data += `<tr>
+                    <th scope="row" class="test-center">${ index }</th>
+                    <td class="text-center">${ contact.Name }</td>
+                    <td class="text-center">${ contact.ContactNumber }</td>
+                    <td class="text-center">${ contact.EmailAddress }</td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                </tr>
+                `
+
+                index++
+            }
+
+            contactList.innerHTML = data
+
+        }
 
     }
 
